c975caabbe02605a7475efc46b9403bcb09782b6
hasheddan <georgedanielmangum@gmail.com>
2020-05-18 14:38:31 -0500

    Update license headers to The Knobs Authors
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 Kris Nóva <kris@nivenly.com>
+Copyright © 2020 The Knobs Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,5 +18,5 @@ package main
 import "github.com/kris-nova/knobs/cmd"
 
 func main() {
-  cmd.Execute()
+	cmd.Execute()
 }
